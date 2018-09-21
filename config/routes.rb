9bea0e3b16a561cd478dc977def1371929f9e176
Thomas Wesley Matlock <snaxbags@protonmail.com>
2018-09-21 21:13:19 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
-      get 'posts/:id', to: 'posts#show'
-  # The priority is based upon order of creation: first created -> highest priority.
+    resources :posts, only: :show
+      # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
